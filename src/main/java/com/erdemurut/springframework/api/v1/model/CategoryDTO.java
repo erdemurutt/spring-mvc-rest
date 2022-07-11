1f75d0efcf44462565925b14eec7003e36d24bac
Erdem Ürüt <erdem.urut@tegsoft.com>
2022-07-11 17:17:41 +0300

    first commit

---
+++ src/main/java/com/erdemurut/springframework/api/v1/model/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.erdemurut.springframework.api.v1.model;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/erdemurut/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.erdemurut.springframework.repositories;
+
+
+import com.erdemurut.springframework.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
